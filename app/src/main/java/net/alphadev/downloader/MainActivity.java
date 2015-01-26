164313d057a085451ade11d3edd994bfa1366703
Jan Seeger <jan@alphadev.net>
2015-01-26 08:51:30 +0100

    move strings to xml.

--- app/src/main/java/net/alphadev/downloader/MainActivity.java
+++ app/src/main/java/net/alphadev/downloader/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends Activity {
         final String filename = uri.getLastPathSegment();
 
         if (filename == null) {
-            Toast.makeText(this, "Are you sure you got the URL right? This is not a scraper!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, R.string.no_filename, Toast.LENGTH_SHORT).show();
             return null;
         }
 
